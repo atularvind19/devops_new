6f8eaf06c8c00a20f04aa62497520a634db112cb
1361 <yako2009@gmail.com>
2019-06-27 15:21:27 +0530

    Genkins test

--- DevopsJava/src/com/jmr/devops/Devops.java
+++ DevopsJava/src/com/jmr/devops/Devops.java
@@ -5,6 +5,7 @@ public class Devops {
 	public static void main(String[] args) {
 		System.out.println("Devops Project test");
 		System.out.println("Devops eclipse to github");
+		System.out.println("Integration with Genkins");
 	}
 
 }
