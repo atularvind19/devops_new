168485dac98ef12c8d12759bfd9e1d99e0a5e110
1361 <yako2009@gmail.com>
2019-06-28 17:07:11 +0530

    28062019

--- DevopsJava/src/com/jmr/devops/Devops.java
+++ DevopsJava/src/com/jmr/devops/Devops.java
@@ -3,11 +3,8 @@ package com.jmr.devops;
 public class Devops {
 
 	public static void main(String[] args) {
-		System.out.println("Devops Project test");
-		System.out.println("Devops eclipse to github");
-		System.out.println("Integration with Genkins Test2");
-		System.out.println("Integration with Genkins Test3");
-		System.out.println("Integration with Genkins Test4");
+		System.out.println("Devops Project test -- Final ");
+		
 	}
 
 }
